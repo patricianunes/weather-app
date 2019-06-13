e57be7f60c358b65ddf65351280c729543a4590e
Patricia Campos <patricia@krint.io>
2019-06-13 10:48:36 +1000

    Updates

---
+++ precache-manifest.9f5593a939537dd4f9269a7693860281.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "40e4257a88efadb4d197d81ff5b046c5",
+    "url": "/weather-app/index.html"
+  },
+  {
+    "revision": "6b6310d83434be7ad4f6",
+    "url": "/weather-app/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "6104708dbb650def3ac7",
+    "url": "/weather-app/static/css/main.b0821397.chunk.css"
+  },
+  {
+    "revision": "6b6310d83434be7ad4f6",
+    "url": "/weather-app/static/js/2.9b3ead1c.chunk.js"
+  },
+  {
+    "revision": "6104708dbb650def3ac7",
+    "url": "/weather-app/static/js/main.30e0864d.chunk.js"
+  },
+  {
+    "revision": "90e6a6c358468ae00209",
+    "url": "/weather-app/static/js/runtime~main.4606d7f5.js"
+  },
+  {
+    "revision": "b6796c732547b6b7e19cdc8e4ad58356",
+    "url": "/weather-app/static/media/bg.b6796c73.jpg"
+  }
+]);
